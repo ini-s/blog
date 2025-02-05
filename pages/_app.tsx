873349e81562d28e68e6b7c277a84a7d72191d86
Inioluwa <inisoetan2014@gmail.com>
2025-02-05 17:39:27 +0100

    refactor(_app.tsx): change Inter font to Roboto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "@/styles/globals.css";
 
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 import type { AppProps } from "next/app";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<main className={inter.className}>
-			<Component {...pageProps} />
-		</main>
-	);
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
