4ed85f85bf17ffe06718743a5e9b5bf845efe14c
Inioluwa <inisoetan2014@gmail.com>
2025-02-06 19:04:01 +0100

    refactor([slug].tsx): import AddComment component

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -11,64 +11,67 @@ import { PortableText } from "@portabletext/react";
 import { RichText } from "@/components/RichText";
 
 import styles from "@/styles/BlogPage.module.css";
+import AddComments from "@/components/AddComments";
 
 interface Props {
-	params: {
-		slug: string;
-	};
+  params: {
+    slug: string;
+  };
 }
 
 export const revalidate = 60;
 
 const SlugPage = ({ post }: { post: Post }) => {
-	return (
-		<div className={styles.postContainer}>
-			<div>
-				<div className={styles.mainImage}>
-					{post?.mainImage && (
-						<Image
-							src={urlFor(post?.mainImage).url()}
-							fill
-							sizes="100%"
-							alt="main image"
-						/>
-					)}
-				</div>
-			</div>
-			<div className={styles.bodyContainer}>
-				<PortableText value={post?.body} components={RichText} />
-			</div>
-		</div>
-	);
+  return (
+    <section className={styles.postContainer}>
+      <div>
+        <div className={styles.mainImage}>
+          {post?.mainImage && (
+            <Image
+              src={urlFor(post?.mainImage).url()}
+              fill
+              sizes="100%"
+              alt="main image"
+            />
+          )}
+        </div>
+      </div>
+      <div className={styles.bodyContainer}>
+        <PortableText value={post?.body} components={RichText} />
+      </div>
+
+      <AddComments postId={post?._id} />
+    </section>
+  );
 };
 
 export default SlugPage;
 
 export async function getStaticPaths() {
-	const paths: string[] = await client.fetch(
-		groq`*[_type == "post" && defined(slug.current)][].slug.current`
-	);
+  const paths: string[] = await client.fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  );
 
-	return {
-		paths: paths.map((slug) => ({ params: { slug } })),
-		fallback: true,
-	};
+  return {
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: true,
+  };
 }
 
 export async function getStaticProps({ params }: Props) {
-	const slug = params.slug;
+  const slug = params.slug;
 
-	const query = groq`*[_type == 'post' && slug.current == $slug][0]{
+  const query = groq`*[_type == 'post' && slug.current == $slug][0]{
 		...,
 		body,
 		author->
 	}`;
 
-	const post: Post = await client.fetch(query, { slug });
+  const post: Post = await client.fetch(query, { slug });
 
-	return {
-		props: {
-			post,
-		},
-	};
+  return {
+    props: {
+      post,
+    },
+  };
 }
