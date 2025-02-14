76bca88d58745fdf9b1dc592db3286784010ed17
Inioluwa <inisoetan2014@gmail.com>
2025-02-14 07:54:51 +0100

    refactor(types.ts): remove email type from Comment

--- types.ts
+++ types.ts
@@ -1,67 +1,66 @@
 type Base = {
-	_createdAt: string;
-	_id: string;
-	_rev: string;
-	_type: string;
-	_updatedAt: string;
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
 };
 
 export interface Post extends Base {
-	author: Author;
-	body: Block[];
-	categories: Category[];
-	mainImage: Image;
-	slug: Slug;
-	title: string;
-	description: string;
-	comments: Comment[];
+  author: Author;
+  body: Block[];
+  categories: Category[];
+  mainImage: Image;
+  slug: Slug;
+  title: string;
+  description: string;
+  comments: Comment[];
 }
 
 interface Author extends Base {
-	description: string;
-	image: Image;
-	name: string;
-	slug: Slug;
+  description: string;
+  image: Image;
+  name: string;
+  slug: Slug;
 }
 
 interface Image {
-	_type: "image";
-	asset: Reference;
+  _type: "image";
+  asset: Reference;
 }
 
 interface Reference {
-	_type: "slug";
-	current: string;
+  _type: "slug";
+  current: string;
 }
 
 interface Slug {
-	_type: "slug";
-	current: string;
+  _type: "slug";
+  current: string;
 }
 
 interface Block {
-	_key: string;
-	_type: "block";
-	children: Span[];
-	markDefs: any[];
-	style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+  _key: string;
+  _type: "block";
+  children: Span[];
+  markDefs: any[];
+  style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
 }
 
 interface Span {
-	_key: string;
-	_type: "span";
-	marks: string[];
-	text: string;
+  _key: string;
+  _type: "span";
+  marks: string[];
+  text: string;
 }
 
 interface Category extends Base {
-	description: string;
-	title: string;
+  description: string;
+  title: string;
 }
 
-interface Comment extends Base {
-	name: string;
-	email: string;
-	comment: string;
-	post: Reference; 
+export interface Comment extends Base {
+  name: string;
+  comment: string;
+  post: Reference;
 }
