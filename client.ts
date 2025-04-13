625ea38e8677907b5de4e689102193000601af04
Inioluwa <inisoetan2014@gmail.com>
2025-04-13 11:53:04 +0100

    refactor(client.ts): remove blank space in token=process.env

--- client.ts
+++ client.ts
@@ -3,18 +3,18 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-const token = process.env. NEXT_PUBLIC_SANITY_TOKEN!;
+const token = process.env.NEXT_PUBLIC_SANITY_TOKEN!;
 
 export const client = createClient({
-	projectId,
-	dataset,
-	apiVersion: "2024-02-02",
-	useCdn: true,
-	token,
+  projectId,
+  dataset,
+  apiVersion: "2024-02-02",
+  useCdn: true,
+  token,
 });
 
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source: any) => {
-	return builder.image(source);
+  return builder.image(source);
 };
