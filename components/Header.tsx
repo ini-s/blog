7bc6db3f753e431816dd11a822f4e3dbcd2e48d6
Inioluwa <inisoetan2014@gmail.com>
2025-04-15 21:54:21 +0100

    refactor(Header.tsx): create header component

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+import ShowView from "./ShowView";
+
+import Sidebar from "./Sidebar";
+
+import Logo from "../public/blog_logo.png";
+
+import styles from "@/styles/Header.module.css";
+
+function Header() {
+  const [active, setIsActive] = useState("home");
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  console.log(openSidebar);
+
+  const router = useRouter();
+
+  return (
+    <header className={styles.headerWrapper}>
+      <div className={styles.logoWrapper}>
+        <Image
+          placeholder="blur"
+          src={Logo}
+          alt="logo"
+          quality={100}
+          onClick={() => router.push("/")}
+        />
+      </div>
+
+      <nav>
+        <div className={styles.menu} onClick={() => setOpenSidebar(true)}>
+          <GiHamburgerMenu />
+        </div>
+
+        <ul className={styles.navList}>
+          <li className={styles.navItem} onClick={() => setIsActive("home")}>
+            <Link href="/" style={{ color: active == "home" ? "#b0b0b0" : "" }}>
+              Home
+            </Link>
+          </li>
+          <li
+            className={styles.navItem}
+            onClick={() => setIsActive("portfolio")}
+          >
+            <a
+              href="https://inioluwasoetan.com.ng/"
+              style={{ color: active == "portfolio" ? "#b0b0b0" : "" }}
+            >
+              Portfolio
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <ShowView when={openSidebar}>
+        <Sidebar closeSidebar={setOpenSidebar} />
+      </ShowView>
+    </header>
+  );
+}
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,29 @@
 import { Toaster } from "sonner";
+import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
 import "@/styles/globals.css";
 
 import { Roboto } from "next/font/google";
-
-import type { AppProps } from "next/app";
 
 const inter = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & { Component: NextPageWithLayout };
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <main className={inter.className}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
       <Toaster position="top-center" richColors />
     </main>
   );
